69061d4d9828098c530582b3a1650bb6edaf7673
duo <tran.khac.binh.duong@azoom.jp>
2024-02-21 17:05:42 +0700

    :tada: Init project

---
+++ app.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const checked = ref(true)
+</script>
+<template>
+  <InputSwitch
+    v-model="checked"
+    :pt="{
+      slider: ({ props }) => ({
+        class: props.modelValue ? 'my-custom-slider -checked' : 'my-custom-slider -unchecked',
+      }),
+      input: 'my-custom-input',
+      root: 'my-custom-root',
+    }"
+  />
+</template>
+<style>
+.my-custom-slider {
+  font-size: 8px;
+  transition: ease-in-out 0.5s;
+  color: white;
+}
+
+.my-custom-slider.-unchecked::after {
+  content: 'OFF';
+  position: absolute;
+  top: 8px;
+  right: 0;
+  margin: 0 4px;
+}
+
+.my-custom-slider.-checked::after {
+  content: 'ON';
+  position: absolute;
+  top: 8px;
+  margin: 0 4px;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    'nuxt-primevue'
+  ],
+  css: ['primevue/resources/themes/aura-light-green/theme.css'],
+  primevue: {
+    options: {
+      ripple: true,
+   }
+  },
+})
